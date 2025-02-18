261894623ceb3310edb01a6c8fd28bfb7fcd90da
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-18 17:47:37 +0530

    Create J03NaturalNumberSum.java

---
+++ 01LogicBuilding/J03NaturalNumberSum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class J03NaturalNumberSum {
+    public static int usingLoop(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    // [Expected Approach] Formula Based Method
+    // Sum of first n natural numbers = (n * (n+1)) / 2
+    public static int usingFormula(int n) {
+        return (n * (n + 1)) / 2;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println(usingLoop(num));
+        System.out.println(usingFormula(num));
+    }
+}
