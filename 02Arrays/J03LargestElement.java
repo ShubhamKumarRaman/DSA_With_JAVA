e32de0805b74d2e57377078d2d9d90032ef01af6
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-21 11:58:25 +0530

    Create J03LargestElement.java

---
+++ 02Arrays/J03LargestElement.java
@@ -0,0 +1,25 @@
+public class J03LargestElement {
+    public static int findUsingIteration(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int element : arr) {
+            if (element > largest) {
+                largest = element;
+            }
+        }
+        return largest;
+    }
+
+    // Recursive Approach – O(n) Time and O(n) Space
+    public static int findUsingRecursion(int arr[], int i){
+        if(i==arr.length-1){
+            return arr[i];
+        }
+        return Math.max(arr[i], findUsingRecursion(arr, i+1));
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 10, 324, 45, 90, 980, 8 };
+        System.out.println("Largest element: " + findUsingIteration(arr));
+        System.out.println("Largest element: " + findUsingRecursion(arr, 0));
+    }
+}
