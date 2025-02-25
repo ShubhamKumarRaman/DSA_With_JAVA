1ce6e3a4fd6a7144fe41cbe2520e4345a7b854e2
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-25 08:38:55 +0530

    Update J08RemoveDuplicate.java

--- 02Arrays/J08RemoveDuplicate.java
+++ 02Arrays/J08RemoveDuplicate.java
@@ -15,12 +15,35 @@ public class J08RemoveDuplicate {
         return index;
     }
 
+    // Expected Approach – O(n) Time and O(1) Space
+    public static int usingPreviousCheck(int arr[]) {
+        int n = arr.length;
+        if (n <= 1) {
+            return n;
+        }
+
+        int index = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[index++] = arr[i];
+            }
+        }
+        return index;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 1, 2, 2, 3, 4, 4, 4, 5, 5 };
         int newSize = usingHashSet(arr);
         System.out.print("Unique elements:- ");
         for (int i = 0; i < newSize; i++) {
             System.out.print(arr[i] + "  ");
+        }
+
+        int arr2[] = { 2, 2, 2, 2, 2 };
+        int newSize2 = usingPreviousCheck(arr2);
+        System.out.print("\nUnique elements:- ");
+        for (int i = 0; i < newSize2; i++) {
+            System.out.print(arr2[i] + "  ");
         }
     }
 }
