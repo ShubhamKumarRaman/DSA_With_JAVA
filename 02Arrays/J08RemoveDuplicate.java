8255b5d4cf0e54de60861433a6670ccdb16eeb10
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-25 08:45:15 +0530

    Update J08RemoveDuplicate.java

--- 02Arrays/J08RemoveDuplicate.java
+++ 02Arrays/J08RemoveDuplicate.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class J08RemoveDuplicate {
@@ -31,6 +32,22 @@ public class J08RemoveDuplicate {
         return index;
     }
 
+    // Check new Previous element and add unique elements
+    public static ArrayList<Integer> usingReturnArrayList(int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (arr.length <= 1) {
+            return result;
+        }
+        result.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                result.add(arr[i]);
+            }
+        }
+        return result;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 1, 2, 2, 3, 4, 4, 4, 5, 5 };
         int newSize = usingHashSet(arr);
@@ -44,6 +61,13 @@ public class J08RemoveDuplicate {
         System.out.print("\nUnique elements:- ");
         for (int i = 0; i < newSize2; i++) {
             System.out.print(arr2[i] + "  ");
+        }
+
+        int arr3[] = { 1, 2, 2, 3, 4, 4, 4, 5, 5 };
+        ArrayList<Integer> result = usingReturnArrayList(arr3);
+        System.out.print("\nUnique elements:- ");
+        for (int x : result) {
+            System.out.print(x + "  ");
         }
     }
 }
