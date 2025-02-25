747afc639784501cef5f6f89ca963e412a51ae76
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-25 08:19:04 +0530

    Update J07CheckShorted.java

--- 02Arrays/J07CheckShorted.java
+++ 02Arrays/J07CheckShorted.java
@@ -1,4 +1,5 @@
 public class J07CheckShorted {
+    // Iterative approach – O(n) Time and O(1) Space
     public static boolean CheckShortedIterative(int arr[]) {
         if (arr.length == 0 || arr.length == 1) {
             return true;
@@ -12,11 +13,21 @@ public class J07CheckShorted {
         return true;
     }
 
+    // Recursive approach – O(n) Time and O(n) Space
+    public static boolean CheckShortedRecursive(int arr[], int n) {
+        if (n == 0 || n == 1) {
+            return true;
+        }
+
+        return (arr[n - 1] >= arr[n - 2]) && CheckShortedRecursive(arr, n - 1);
+    }
+
     public static void main(String args[]) {
         int arr[] = { 20, 23, 23, 45, 78, 88 };
-        System.out.println("arr:- "+CheckShortedIterative(arr));
-        int arr2[] = {20, 20, 78, 98, 99, 97};
-        System.out.println("arr2:- "+CheckShortedIterative(arr2));
-
+        System.out.println("arr:- " + CheckShortedIterative(arr));
+        System.out.println("arr:- " + CheckShortedRecursive(arr, arr.length));
+        int arr2[] = { 20, 20, 78, 98, 99, 97 };
+        System.out.println("arr2:- " + CheckShortedIterative(arr2));
+        System.out.println("arr2:- " + CheckShortedRecursive(arr2, arr2.length));
     }
 }
