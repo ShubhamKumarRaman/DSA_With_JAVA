98c3865fda6a04e1ced45307436722d92f0e2da0
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-05 23:05:31 +0530

    Create J05ClosestNumber.java

---
+++ 01LogicBuilding/J05ClosestNumber.java
@@ -0,0 +1,16 @@
+public class J05ClosestNumber {
+    // [Naive Approach] Iterative Checking
+    public static int closestNumberIterative(int num, int div) {
+        int closest = 0;
+        int minDifference = Integer.MAX_VALUE;
+
+        for (int i = num - Math.abs(div); i <= num + Math.abs(div); i++) {
+            
+        }
+    }
+
+    public static void main(String args[]) {
+        int num, div;
+
+    }
+}
