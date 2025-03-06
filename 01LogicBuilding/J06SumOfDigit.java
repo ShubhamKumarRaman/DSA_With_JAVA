d9521b0a2de2c2b4605dc543807ad0dc70ccd119
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-06 08:55:42 +0530

    Update J06SumOfDigit.java

--- 01LogicBuilding/J06SumOfDigit.java
+++ 01LogicBuilding/J06SumOfDigit.java
@@ -10,7 +10,16 @@ public class J06SumOfDigit {
         return sum;
     }
 
+    // Recursive Approach
+    public static int usingRecursive(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return (num % 10) + (usingRecursive(num / 10));
+    }
+
     public static void main(String args[]) {
-        System.out.println("Sum is : "+usingIterative(1325));
+        System.out.println("Sum is : " + usingIterative(1325));
+        System.out.println("Sum is : " + usingRecursive(1562));
     }
 }
