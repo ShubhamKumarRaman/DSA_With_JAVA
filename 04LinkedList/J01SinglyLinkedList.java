736b62c42e643bdaeacc9a85b091ccae2e387557
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-07 20:07:02 +0530

    Create J01SinglyLinkedList.java

---
+++ 04LinkedList/J01SinglyLinkedList.java
@@ -0,0 +1,24 @@
+
+public class J01SinglyLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public static void main(String args[]) {
+        int choice;
+        System.out.println("0. Exit.");
+        
+        System.out.println("Enter your choice: ");
+
+    }
+}
