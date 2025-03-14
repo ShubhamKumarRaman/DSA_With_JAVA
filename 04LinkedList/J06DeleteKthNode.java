c13ddaf35f7e79dbb1fcb9bc7ca0abe1c816aefa
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-14 21:23:28 +0530

    Create J06DeleteKthNode.java

---
+++ 04LinkedList/J06DeleteKthNode.java
@@ -0,0 +1,17 @@
+public class J06DeleteKthNode {
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // [Expected Approach – 1] Iterative Approach – O(n) Time and O(1) Space
+
+    public public static void main(String args[]) {
+        
+    }
+}
