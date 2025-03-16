ceb1245edfb49944199a96653376018763ce8ec9
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-16 13:41:32 +0530

    Update J06DeleteKthNode.java

--- 04LinkedList/J06DeleteKthNode.java
+++ 04LinkedList/J06DeleteKthNode.java
@@ -1,5 +1,5 @@
 public class J06DeleteKthNode {
-    public class Node {
+    public static class Node {
         int data;
         Node next;
 
@@ -10,8 +10,32 @@ public class J06DeleteKthNode {
     }
 
     // [Expected Approach – 1] Iterative Approach – O(n) Time and O(1) Space
+    public static Node usingIterative(Node head, int k) {
+        Node current = head;
+        Node previous = null;
+        int count = 0;
+        while (current != null) {
+            count++;
+            if (count % k == 0) {
+                if (previous != null) {
+                    previous.next = current.next;
+                } else {
+                    head = current.next;
+                }
+            } else {
+                previous = current;
+            }
+            current = current.next;
+        }
+        return head;
+    }
 
-    public public static void main(String args[]) {
-        
+    public static void main(String args[]) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
     }
 }
