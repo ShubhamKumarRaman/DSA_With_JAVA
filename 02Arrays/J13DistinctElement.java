b89df88b3aa9eae9660a64b864d7c1d9e8c4c7bf
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-17 08:45:58 +0530

    Update J13DistinctElement.java

--- 02Arrays/J13DistinctElement.java
+++ 02Arrays/J13DistinctElement.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class J13DistinctElement {
     // [Naive Approach] Using Nested loops – O(n^2) Time and O(1) Space
@@ -19,6 +20,18 @@ public class J13DistinctElement {
         return result;
     }
 
+    // [Better Approach] Using Sorting – O(n*logn) Time and O(1) Space
+    public static ArrayList<Integer> usingSorting(int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0 || arr[i] != arr[i - 1]) {
+                result.add(arr[i]);
+            }
+        }
+        return result;
+    }
+
     // Print List
     public static void printList(ArrayList<Integer> result) {
         for (int x : result) {
@@ -30,6 +43,9 @@ public class J13DistinctElement {
     public static void main(String args[]) {
         int arr[] = { 12, 10, 9, 45, 2, 10, 10, 45 };
         ArrayList<Integer> result = usingNestedLoop(arr);
+        printList(result);
+        int arr2[] = { 1, 1, 1, 2, 1, 1 };
+        result = usingSorting(arr2);
         printList(result);
     }
 }
