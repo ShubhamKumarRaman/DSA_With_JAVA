ddd6ad50002b356ccd002a752475ee334c59e298
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-20 08:34:49 +0530

    Update J09TraverseCircularLinkedList.java

--- 04LinkedList/J09TraverseCircularLinkedList.java
+++ 04LinkedList/J09TraverseCircularLinkedList.java
@@ -9,6 +9,7 @@ public class J09TraverseCircularLinkedList {
         }
     }
 
+    // [Expected Approach – 1] Using Iterative Method – O(n) Time and O(1) Space:
     public static void usingIteration(Node head) {
         if (head == null) {
             return;
@@ -21,6 +22,18 @@ public class J09TraverseCircularLinkedList {
         System.out.println("null");
     }
 
+    // [Expected Approach – 2] Using Recursion – O(n) Time and O(n) Space:
+    public static void usingRecursion(Node head, Node current) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(" -> " + current.data);
+        if (current.next == head) {
+            return;
+        }
+        usingRecursion(head, current.next);
+    }
+
     public static void main(String args[]) {
         Node head = new Node(11);
         head.next = new Node(2);
@@ -29,5 +42,6 @@ public class J09TraverseCircularLinkedList {
         head.next.next.next.next = head;
 
         usingIteration(head);
+        usingRecursion(head, head);
     }
 }
