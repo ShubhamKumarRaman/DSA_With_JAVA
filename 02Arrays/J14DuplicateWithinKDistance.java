bba05013c9cf30bb1237f46dc6e60bce2840df64
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-21 09:10:47 +0530

    Update J14DuplicateWithinKDistance.java

--- 02Arrays/J14DuplicateWithinKDistance.java
+++ 02Arrays/J14DuplicateWithinKDistance.java
@@ -11,11 +11,28 @@ public class J14DuplicateWithinKDistance {
         return false;
     }
 
-    
+    static boolean checkDuplicatesWithinK2(int[] arr, int k) {
+        int n = arr.length;
+
+        // Traverse for every element
+        for (int i = 0; i < n; i++) {
+
+            // Traverse next k elements
+            for (int c = 1; c <= k && (i + c) < n; c++) {
+                int j = i + c;
+
+                // If we find one more occurrence within k
+                if (arr[i] == arr[j])
+                    return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String args[]) {
         int[] arr = { 10, 5, 3, 4, 3, 5, 6 };
         System.out.println(CheckDuplicateWithinKDistance(arr, 3) ? "Yes" : "No");
-        
+        int[] arr2 = { 1, 2, 3, 4, 1, 2, 3, 4 };
+        System.out.println(checkDuplicatesWithinK2(arr2, 3) ? "Yes" : "No");
     }
 }
