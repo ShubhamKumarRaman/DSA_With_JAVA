072335bc194f19c3bdec77a74b3b1568257ac899
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-25 21:29:28 +0530

    Update J10CheckCircularLL.java

--- 04LinkedList/J10CheckCircularLL.java
+++ 04LinkedList/J10CheckCircularLL.java
@@ -14,12 +14,22 @@ public class J10CheckCircularLL {
         if (head == null)
             return true;
         Node temp = head;
-        while(temp!=null || temp!=head){
-            
+        while (temp.next != null && temp.next != head) {
+            temp = temp.next;
         }
+        if (temp == null || temp.next == null) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String args[]) {
-
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        System.out.println(TraverseAllNode(head) ? "Yes" : "No");
+        head.next.next.next.next = head;
+        System.out.println(TraverseAllNode(head) ? "Yes" : "No");
     }
 }
