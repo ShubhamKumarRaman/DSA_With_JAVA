d72c7b9e083dc492d0a7ac3abcaf584f68ba948a
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-28 21:32:03 +0530

    Update J08PrimeNumber.java

--- 01LogicBuilding/J08PrimeNumber.java
+++ 01LogicBuilding/J08PrimeNumber.java
@@ -13,9 +13,24 @@ public class J08PrimeNumber {
         return true;
     }
 
-    public static void main(String args[]) {
-        System.out.println(usingSchoolMethod(5));
-        System.out.println(usingSchoolMethod(6));
+    // Optimized School Method
+    public static boolean usingOptSchoolMethod(int n) {
+        if (n <= 1) {
+            return false;
+        }
 
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        System.out.println("5 " + usingSchoolMethod(5));
+        System.out.println("6 " + usingSchoolMethod(6));
+        System.out.println("16 " + usingOptSchoolMethod(16));
+        System.out.println("17 " + usingOptSchoolMethod(17));
     }
 }
