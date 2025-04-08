1b4b2e068dce606bf89e6bf758affcfdb2d6cca8
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-08 21:25:40 +0530

    Create J02StringEquals.java

---
+++ 03String/J02StringEquals.java
@@ -0,0 +1,11 @@
+public class J02StringEquals {
+    public static void main(String args[]) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = "Hey";
+
+        // Using Equals method
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+    }
+}
