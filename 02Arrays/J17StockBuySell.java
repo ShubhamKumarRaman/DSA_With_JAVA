e3ad9154c7c827397085d069f7cbd7a8b0065490
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-18 09:18:38 +0530

    using recursion stock buy and sell

--- 02Arrays/J17StockBuySell.java
+++ 02Arrays/J17StockBuySell.java
@@ -1,4 +1,5 @@
 public class J17StockBuySell {
+    // Using check everday's Profit
     public static int usingCheckProfit(int price[]) {
         int profit = 0;
         for (int i = 0; i < price.length - 1; i++) {
@@ -9,8 +10,28 @@ public class J17StockBuySell {
         return profit;
     }
 
+    // [Naive Approach] Using Recursion – Exponential Time
+    public static int usingRecursion(int prices[]) {
+        return maxProfitRec(prices, 0, prices.length - 1);
+    }
+
+    public static int maxProfitRec(int[] prices, int start, int end) {
+        int profit = 0;
+        for (int i = start; i < end; i++) {
+            for (int j = i + 1; j <= end; j++) {
+                if (prices[j] > prices[i]) {
+                    int current = (prices[j] - prices[i]) + maxProfitRec(prices, start, i - 1)
+                            + maxProfitRec(prices, j + 1, end);
+                    profit = Math.max(profit, current);
+                }
+            }
+        }
+        return profit;
+    }
+
     public static void main(String args[]) {
         int[] prices = { 100, 180, 260, 310, 40, 535, 695 };
         System.out.println("Maximum Profit is: " + usingCheckProfit(prices));
+        System.out.println("Maximum Profit is: "+usingRecursion(prices));
     }
 }
