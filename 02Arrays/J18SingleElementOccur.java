1c391102483f826c4fb04f57996fa168566b2bb3
ShubhamKumarRaman <shubham620409@gmail.com>
2025-04-21 13:08:19 +0530

    Update J18SingleElementOccur.java

--- 02Arrays/J18SingleElementOccur.java
+++ 02Arrays/J18SingleElementOccur.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class J18SingleElementOccur {
     // Naive Approach – O(n^2) Time and O(1) Space
     public static int SearchSingleElement(int arr[]) {
@@ -15,9 +18,25 @@ public class J18SingleElementOccur {
         return -1;
     }
 
-    
+    // Better Approach- Using Hash Map – O(n) Time and O(n) Space
+    public static int usingHashMap(int arr[]) {
+        int n = arr.length;
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            cnt.put(arr[i], cnt.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> p : cnt.entrySet()) {
+            if (p.getValue() == 1) {
+                return p.getKey();
+            }
+        }
+        return -1;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 2, 3, 5, 4, 5, 2, 4 };
         System.out.println("Single Element: " + SearchSingleElement(arr));
+        System.out.println("Using HashMap: "+usingHashMap(arr));
     }
 }
