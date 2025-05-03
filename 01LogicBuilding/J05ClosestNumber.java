d16196ddc960ef3f05ffa9286f15b7094372dc46
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-03 21:37:54 +0530

    Update J05ClosestNumber.java

--- 01LogicBuilding/J05ClosestNumber.java
+++ 01LogicBuilding/J05ClosestNumber.java
@@ -1,5 +1,6 @@
 public class J05ClosestNumber {
-    // [Naive Approach] Iterative Checking Time Complexity – O(m) Space Complexity – O(1)
+    // [Naive Approach] Iterative Checking Time Complexity – O(m) Space Complexity –
+    // O(1)
     public static int closestNumberIterative(int num, int div) {
         int closest = 0;
         int minDifference = Integer.MAX_VALUE;
@@ -18,8 +19,23 @@ public class J05ClosestNumber {
         return closest;
     }
 
+    // [Expected Approach] By finding Quotient
+    public static int usingFindingQuotient(int num, int div) {
+        int q = num / div;
+
+        int n1 = div * q;
+
+        int n2 = (div * q) > 0 ? (div * (q + 1)) : (div * (q - 1));
+
+        if (Math.abs(num - n1) < Math.abs(num - n2)) {
+            return n1;
+        }
+        return n2;
+    }
+
     public static void main(String args[]) {
         int num = 13, div = 4;
         System.out.println(closestNumberIterative(num, div));
+        System.out.println(usingFindingQuotient(num, div));
     }
 }
