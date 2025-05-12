24331caa0e1fa3935d23ef7b8e9576d804e7824b
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-12 08:12:11 +0530

    Update J19MissingNumber.java

--- 02Arrays/J19MissingNumber.java
+++ 02Arrays/J19MissingNumber.java
@@ -18,8 +18,25 @@ public class J19MissingNumber {
         return -1;
     }
 
+    // [Better Approach] Using Hashing – O(n) Time and O(n) Space
+    public static int usingHashing(int arr[]) {
+        int n = arr.length + 1;
+        int hash[] = new int[n + 1];
+        for (int i = 0; i < n - 1; i++) {
+            hash[arr[i]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (hash[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 8, 2, 4, 5, 3, 7, 1 };
         System.out.println("Missing Number: " + usingLinearSearch(arr));
+        System.out.println("Missing Number: " + usingHashing(arr));
     }
 }
