eca6df120c5419722e80bc2192c51ddb8433e3b6
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-16 21:47:31 +0530

    Create J12DeletionCircularLinkedList.java

---
+++ 04LinkedList/J12DeletionCircularLinkedList.java
@@ -0,0 +1,23 @@
+public class J12DeletionCircularLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node deleteFirst(Node head) {
+        if (head == null) {
+            System.out.println("List is Empty.");
+            return null;
+        }
+        
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
