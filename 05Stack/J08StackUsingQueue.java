888a07cc44e66ede651288358dcdc1d0d9ce8264
ShubhamKumarRaman <shubham620409@gmail.com>
2025-05-31 13:57:40 +0530

    Create J08StackUsingQueue.java

---
+++ 05Stack/J08StackUsingQueue.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class J08StackUsingQueue {
+    // By making push() operation costly - Push in O(n) and Pop() in O(1)
+    public static class stack1 {
+        static Queue<Integer> q1 = new LinkedList<Integer>();
+        static Queue<Integer> q2 = new LinkedList<Integer>();
+
+        static int currentSize = 0;
+
+        public void push(int data) {
+            currentSize++;
+            while (!q1.isEmpty()) {
+                q2.add(q1.peek());
+                q1.remove();
+            }
+            q1.add(data);
+            while (!q2.isEmpty()) {
+                q1.add(q2.peek());
+                q2.remove();
+            }
+        }
+
+        public int pop() {
+            if (q1.isEmpty()) {
+                System.out.println("Stack is Empty.");
+                return -1;
+            }
+            currentSize--;
+            int data = q1.peek();
+            q1.remove();
+            return data;
+        }
+
+        public void printStack() {
+            System.out.println("Stack elements are: " + q1);
+        }
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
