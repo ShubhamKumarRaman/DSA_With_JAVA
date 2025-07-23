c37b3fd0b16314d44e2e02a8fff7312e294dc3b7
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-23 13:36:37 +0530

    Update J20RepeatingMissing.java

--- 02Arrays/J20RepeatingMissing.java
+++ 02Arrays/J20RepeatingMissing.java
@@ -78,6 +78,69 @@ public class J20RepeatingMissing {
         return result;
     }
 
+    // [Approach 4] Using XOR - O(n) Time and O(1) Space
+    public static ArrayList<Integer> usingXOR(int[] arr) {
+        int n = arr.length;
+        int xorVal = 0;
+
+        // get the xor of all array elements
+        // And numbers from 1 to n
+        for (int i = 0; i < n; i++) {
+            xorVal ^= arr[i];
+            xorVal ^= (i + 1); // 1 to n numbers
+        }
+
+        // get the rightmost set bit in xorVal
+        int setBitIndex = xorVal & ~(xorVal - 1);
+
+        int x = 0, y = 0;
+
+        // now divide elements into two sets
+        // by comparing rightmost set bit
+        for (int i = 0; i < n; i++) {
+
+            // decide whether arr[i] is in first set
+            // or second
+            if ((arr[i] & setBitIndex) != 0) {
+                x ^= arr[i];
+            } else {
+                y ^= arr[i];
+            }
+
+            // decide whether (i+1) is in first set
+            // or second
+            if (((i + 1) & setBitIndex) != 0) {
+                x ^= (i + 1);
+            } else {
+                y ^= (i + 1);
+            }
+        }
+
+        // x and y are the repeating and missing values.
+        // to know which one is what, traverse the array
+        int missing, repeating;
+
+        int xCnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                xCnt++;
+            }
+        }
+
+        if (xCnt == 0) {
+            missing = x;
+            repeating = y;
+        } else {
+            missing = y;
+            repeating = x;
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(repeating);
+        result.add(missing);
+        return result;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 3, 1, 3 };
         ArrayList<Integer> ans = usingVisitedArray(arr);
@@ -89,6 +152,10 @@ public class J20RepeatingMissing {
 
         int[] arr3 = { 3, 1, 3 };
         ans = usingMathEquation(arr3);
+        System.out.println("Repeating:- " + ans.get(0) + " " + "Missing:- " + ans.get(1));
+
+        int[] arr4 = { 3, 1, 3 };
+        ans = usingXOR(arr4);
         System.out.println("Repeating:- " + ans.get(0) + " " + "Missing:- " + ans.get(1));
     }
 }
