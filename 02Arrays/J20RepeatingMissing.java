69c92f0c3b4652bfbd06ca9b2e8c627ccbd1f16f
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-23 13:31:56 +0530

    Update J20RepeatingMissing.java

--- 02Arrays/J20RepeatingMissing.java
+++ 02Arrays/J20RepeatingMissing.java
@@ -57,12 +57,38 @@ public class J20RepeatingMissing {
         return result;
     }
 
+    // [Approach 3] Making Two Math Equations - O(n) Time and O(1) Space
+    public static ArrayList<Integer> usingMathEquation(int[] arr) {
+        int n = arr.length;
+
+        int s = (n * (n + 1)) / 2;
+        int ssq = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        for (int i = 0; i < n; i++) {
+            s -= arr[i];
+            ssq -= arr[i] * arr[i];
+        }
+
+        int missing = (s + ssq / s) / 2;
+        int repeating = missing - s;
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(repeating);
+        result.add(missing);
+        return result;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 3, 1, 3 };
         ArrayList<Integer> ans = usingVisitedArray(arr);
         System.out.println("Repeating:- " + ans.get(0) + " " + "Missing:- " + ans.get(1));
+
         int[] arr2 = { 4, 3, 6, 2, 1, 1 };
         ans = usingArrayMarking(arr2);
+        System.out.println("Repeating:- " + ans.get(0) + " " + "Missing:- " + ans.get(1));
+
+        int[] arr3 = { 3, 1, 3 };
+        ans = usingMathEquation(arr3);
         System.out.println("Repeating:- " + ans.get(0) + " " + "Missing:- " + ans.get(1));
     }
 }
