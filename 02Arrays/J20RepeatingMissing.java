3816d5e5f1da4d85f3d5786ef2c2b850af74a3ef
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-23 12:51:02 +0530

    Update J20RepeatingMissing.java

--- 02Arrays/J20RepeatingMissing.java
+++ 02Arrays/J20RepeatingMissing.java
@@ -27,9 +27,42 @@ public class J20RepeatingMissing {
         return result;
     }
 
+    // [Approach 2] Using Array Marking - O(n) Time and O(1) Space
+    public static ArrayList<Integer> usingArrayMarking(int[] arr) {
+        int n = arr.length;
+        int repeating = -1;
+
+        for (int i = 0; i < n; i++) {
+            int val = Math.abs(arr[i]);
+
+            if (arr[val - 1] > 0) {
+                arr[val - 1] = -arr[val - 1];
+            } else {
+                repeating = val;
+            }
+        }
+
+        int missing = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(repeating);
+        result.add(missing);
+
+        return result;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 3, 1, 3 };
         ArrayList<Integer> ans = usingVisitedArray(arr);
+        System.out.println("Repeating:- " + ans.get(0) + " " + "Missing:- " + ans.get(1));
+        int[] arr2 = { 4, 3, 6, 2, 1, 1 };
+        ans = usingArrayMarking(arr2);
         System.out.println("Repeating:- " + ans.get(0) + " " + "Missing:- " + ans.get(1));
     }
 }
