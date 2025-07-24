d067f01ce49866580fff3eb38c471ec0318ddfed
ShubhamKumarRaman <shubham620409@gmail.com>
2025-07-24 12:57:06 +0530

    using Hashset

--- 02Arrays/J21OnlyRepeating.java
+++ 02Arrays/J21OnlyRepeating.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class J21OnlyRepeating {
     // [Naive Approach] Using Nested Loop- O(n^2) Time and O(1) Space
@@ -26,9 +27,24 @@ public class J21OnlyRepeating {
         return -1;
     }
 
+    // [Better Approach 2] Hash Set - O(n) Time and O(n) Space
+    public static int usingHashSet(int arr[]) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : arr) {
+            if (set.contains(n)) {
+                return n;
+            }
+            set.add(n);
+        }
+        return -1;
+    }
+
     public static void main(String args[]) {
         int[] arr = { 1, 3, 2, 3, 4 };
         System.out.println("Repeating Number: " + usingNestedLoop(arr));
         System.out.println("Repeating Number: " + usingSorting(arr));
+        System.out.println("Repeating Number: " + usingHashSet(arr));
+        int[] arr2 = { 1, 5, 1, 2, 3, 4 };
     }
 }
