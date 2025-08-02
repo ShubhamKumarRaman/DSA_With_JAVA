593b96750b81a272b5772c456750203968d31e3a
ShubhamKumarRaman <shubham620409@gmail.com>
2025-08-02 07:23:37 +0530

    Delete First Node

--- 04LinkedList/J12DeletionCircularLinkedList.java
+++ 04LinkedList/J12DeletionCircularLinkedList.java
@@ -9,15 +9,49 @@ public class J12DeletionCircularLinkedList {
         }
     }
 
-    public static Node deleteFirst(Node head) {
-        if (head == null) {
+    public static Node deleteFirst(Node last) {
+        if (last == null) {
             System.out.println("List is Empty.");
             return null;
         }
-        
+
+        Node head = last.next;
+        if (head == last) {
+            last = null;
+        } else {
+            last.next = head.next;
+        }
+        return last;
+    }
+
+    public static void printList(Node last) {
+        if (last == null) {
+            System.out.println("List is Empty.");
+            return;
+        }
+        Node current = last.next;
+        while (true) {
+            System.out.print(" -> " + current.data);
+            current = current.next;
+            if (current == last.next) {
+                break;
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String args[]) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next = new Node(40);
+
+        Node last = head.next.next;
+        last.next = head;
+
+        printList(last);
+        deleteFirst(last);
+        printList(last);
 
     }
 }
