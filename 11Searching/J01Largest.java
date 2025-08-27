6a8c23301380cfffc91f92a109ca5dc6aa9cf9a9
Shubham Kumar <shubham620409@gmail.com>
2025-08-27 12:54:25 +0530

    Update J01Largest.java

--- 11Searching/J01Largest.java
+++ 11Searching/J01Largest.java
@@ -10,9 +10,22 @@ public class J01Largest {
         return max;
     }
 
+    // Recursive Approach - O(n) Time and O(n) Space
+    public static int usingRecursive(int[] arr){
+        return findMax(arr, 0);
+    }
+    public static int findMax(int[] arr, int i ){
+        if(i==arr.length-1){
+            return arr[i];
+        }
+
+        return Math.max(arr[i], findMax(arr, i+1));
+    }
+
     public static void main(String args[]) {
-        int arr[] = { 10, 324, 45, 90, 98, 8 };
+        int arr[] = { 10, 324, 45, 990, 98, 8 };
 
         System.out.println("Using Iterative approach : " + usingIterative(arr));
+        System.out.println("Using Recursive approach : "+ usingRecursive(arr));
     }
 }
