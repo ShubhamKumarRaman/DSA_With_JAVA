cbf18c2c8abfb69b329f7ce039d9721160fb2627
Shubham Kumar <shubham620409@gmail.com>
2025-10-22 12:54:46 +0530

    Using HashSet

---
+++ 13Hashing/J01CheckSubSet.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class J01CheckSubSet {
+
+    // My approach - applying hashset -- This approach is valid for unique element
+    // subset
+    public static boolean usingHashSet(int[] arr, int[] sub) {
+        Set<Integer> set = new HashSet<>();
+        // Insert each element
+        for (int n : arr) {
+            set.add(n);
+        }
+
+        // Iterate sub and check every element is present in arr or not
+        for (int n : sub) {
+            if (!set.contains(n)) {
+                return false;
+            }
+            set.remove(n);
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        int[] a = { 11, 1, 13, 21, 3, 7 };
+        int[] b = { 11, 3, 7, 1 };
+
+        System.out.println("Subset: " + usingHashSet(a, b));
+
+        int[] c = { 10, 5, 2, 23, 19 };
+        int[] d = { 19, 5, 3 };
+
+        System.out.println("SubSet: " + usingHashSet(c, d));
+
+        int[] e = { 1, 2, 2 };
+        int[] f = { 1, 1 };
+        System.out.println("Subset: " + usingHashSet(e, f));
+    }
+}
