acb1695726ce4a1bc724a2c3a8e70c17e9818622
Shubham Kumar <shubham620409@gmail.com>
2025-10-26 18:47:04 +0530

    Count occurrence of each element

--- 13Hashing/J03CheckEquals.java
+++ 13Hashing/J03CheckEquals.java
@@ -1,6 +1,10 @@
 
 // Check if two arrays are equal or not
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class J03CheckEquals {
 
@@ -20,13 +24,39 @@ public class J03CheckEquals {
         return true;
     }
 
+    // Using Hashing with count Occurrence
+    public static boolean usingCountOccurrence(int[] a, int[] b) {
+        if (a.length != b.length)
+            return false;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int el : a) {
+            freq.put(el, freq.getOrDefault(el, 0) + 1);
+        }
+
+        for (int el : b) {
+            if (!freq.containsKey(el) || freq.get(el) == 0) {
+                return false;
+            }
+            freq.put(el, freq.get(el) - 1);
+        }
+
+        for (int count : freq.values()) {
+            if (count != 0)
+                return false;
+        }
+
+        return true;
+    }
+
     public static void main(String args[]) {
         int[] a = { 3, 5, 2, 5, 2 };
         int[] b = { 2, 3, 5, 5, 2 };
         System.out.println("Using Sorting: " + usingSorting(a, b));
+        System.out.println("Using Count Occurrence: " + usingCountOccurrence(a, b));
         System.out.println();
         int[] c = { 1, 7, 1 };
         int[] d = { 7, 7, 1 };
         System.out.println("Using Sorting: " + usingSorting(c, d));
+        System.out.println("Using Count Occurrence: " + usingCountOccurrence(c, d));
     }
 }
