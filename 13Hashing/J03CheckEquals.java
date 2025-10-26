d87a923e9255b0fb491c61bcebe5b6862069c07f
Shubham Kumar <shubham620409@gmail.com>
2025-10-26 19:05:14 +0530

    Using Hashmap

--- 13Hashing/J03CheckEquals.java
+++ 13Hashing/J03CheckEquals.java
@@ -48,15 +48,48 @@ public class J03CheckEquals {
         return true;
     }
 
+    // [Expected Approach] Using Hashing- O(n) Time and O(n) Space
+    public static boolean usingHashMap(int[] a, int[] b) {
+        if (a.length != b.length)
+            return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int count = 0;
+        for (int el : a) {
+            if (map.get(el) == null) {
+                map.put(el, 1);
+            } else {
+                count = map.get(el);
+                count++;
+                map.put(el, count);
+            }
+        }
+
+        for (int el : b) {
+            if (!map.containsKey(el))
+                return false;
+            if (map.get(el) == 0)
+                return false;
+
+            count = map.get(el);
+            count--;
+            map.put(el, count);
+        }
+        return true;
+    }
+
     public static void main(String args[]) {
         int[] a = { 3, 5, 2, 5, 2 };
         int[] b = { 2, 3, 5, 5, 2 };
         System.out.println("Using Sorting: " + usingSorting(a, b));
         System.out.println("Using Count Occurrence: " + usingCountOccurrence(a, b));
+        System.out.println("Using Hashing: " + usingHashMap(a, b));
         System.out.println();
         int[] c = { 1, 7, 1 };
         int[] d = { 7, 7, 1 };
         System.out.println("Using Sorting: " + usingSorting(c, d));
         System.out.println("Using Count Occurrence: " + usingCountOccurrence(c, d));
+        System.out.println("Using Hashing: " + usingHashMap(c, d));
     }
 }
