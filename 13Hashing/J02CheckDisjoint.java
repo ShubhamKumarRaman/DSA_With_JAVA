49c7376d639b0b03b4b84979714b8bc318bd74d8
Shubham Kumar <shubham620409@gmail.com>
2025-10-26 09:20:18 +0530

    Using Sorting

--- 13Hashing/J02CheckDisjoint.java
+++ 13Hashing/J02CheckDisjoint.java
@@ -1,3 +1,8 @@
+// Check for Disjoint Arrays or Sets
+// Given two arrays a and b, check if they are disjoint, i.e., there is no element common between both the arrays.
+
+import java.util.Arrays;
+
 public class J02CheckDisjoint {
     // [Naive Approach] Using Two Nested Loops - O(n x m) Time and O(1) Space
     public static boolean usingNestedLoop(int[] a, int[] b) {
@@ -11,13 +16,37 @@ public class J02CheckDisjoint {
         return true;
     }
 
+    // [Better Approach] Using Merge of Merge Sort - O(n Log n + m Log m) Time and
+    // O(1) Space
+    public static boolean usingSorting(int[] a, int[] b) {
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int i = 0, j = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j])
+                return false;
+
+            if (a[i] < b[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return true;
+    }
+
     public static void main(String args[]) {
         int[] a = { 12, 34, 11, 9, 3 };
         int[] b = { 7, 2, 1, 5 };
-        System.out.println("Using Nested Loop: "+usingNestedLoop(a, b));
+        System.out.println("Using Nested Loop: " + usingNestedLoop(a, b));
+        System.out.println("Using Sorting: " + usingSorting(a, b));
 
-        int[] c = {12, 34, 11, 9, 3};
-        int[] d = {2, 1, 3, 5};
-        System.out.println("Using Nested Loop: "+usingNestedLoop(c, d));
+        System.out.println();
+
+        int[] c = { 12, 34, 11, 9, 3 };
+        int[] d = { 2, 1, 3, 5 };
+        System.out.println("Using Nested Loop: " + usingNestedLoop(c, d));
+        System.out.println("Using Sorting: " + usingSorting(c, d));
     }
 }
