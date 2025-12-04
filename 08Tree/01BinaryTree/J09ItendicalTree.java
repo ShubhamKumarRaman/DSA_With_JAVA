fbb9edce2bfe6b5949bacd04c103bf36969ff3e2
Shubham Kumar <shubham620409@gmail.com>
2025-12-04 13:13:46 +0530

    Create J09ItendicalTree.java

---
+++ 08Tree/01BinaryTree/J09ItendicalTree.java
@@ -0,0 +1,26 @@
+public class J09ItendicalTree {
+    public class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+    // Approach 1- Traverse and retrieve data of each node
+    public static boolean usingTraversing(Node n1, Node n2){
+
+    }
+    public static int dataNode(Node node){
+        if(node==null){
+            return 0;
+        }
+        
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
