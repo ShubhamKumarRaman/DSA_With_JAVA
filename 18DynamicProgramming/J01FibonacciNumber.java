c0d1aefa67db44dbc30684d30dafccc8ce7eeda9
Shubham Kumar <shubham620409@gmail.com>
2026-01-10 14:51:53 +0530

    Using Tabulation

--- 18DynamicProgramming/J01FibonacciNumber.java
+++ 18DynamicProgramming/J01FibonacciNumber.java
@@ -27,10 +27,28 @@ public class J01FibonacciNumber {
         return fibonacci2(n, memo);
     }
 
+    // [Expected Approach-2] Bottom-Up Approach
+    public static int nthFibonacci3(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String args[]) {
         System.out.println("Using Recursion:- " + nthFibonacci1(5));
-        System.out.println("Using Memoization:- "+nthFibonacci2(5));
+        System.out.println("Using Memoization:- " + nthFibonacci2(5));
+        System.out.println("Using Tabulation:- " + nthFibonacci3(5));
         System.out.println("Using Recursion:- " + nthFibonacci1(8));
-        System.out.println("Using Memoization:- "+nthFibonacci2(8));
+        System.out.println("Using Memoization:- " + nthFibonacci2(8));
+        System.out.println("Using Tabulation:- " + nthFibonacci3(8));
     }
 }
