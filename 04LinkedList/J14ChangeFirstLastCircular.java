a5b9a0c9ae754a07e46792e4386f56bb5d7ee8ac
Shubham Kumar <shubham620409@gmail.com>
2026-02-13 07:48:26 +0530

    Create J14ChangeFirstLastCircular.java

---
+++ 04LinkedList/J14ChangeFirstLastCircular.java
@@ -0,0 +1,47 @@
+public class J14ChangeFirstLastCircular {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void printList(Node head) {
+        Node current = head;
+        do {
+            System.out.print(" -> " + current.data);
+            current = current.next;
+        } while (current != head);
+        System.out.println();
+    }
+
+
+    // Method 2: (By Swapping Values of First and Last nodes)
+    public static Node changeFirstLast(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node current = head;
+        do {
+            current = current.next;
+        } while (current.next != head);
+
+        head.data = head.data + current.data - (current.data = head.data);
+        return head;
+    }
+
+    public static void main(String args[]) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+        head.next.next.next.next.next = head;
+        printList(head);
+        changeFirstLast(head);
+        printList(head);
+    }
+}
