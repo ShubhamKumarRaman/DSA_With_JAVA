e707a1060b16a793dcc55f06d05e6bf0ce322a67
Shubham Kumar <shubham620409@gmail.com>
2026-05-01 14:46:29 +0530

    Using two pointer

--- 14TwoPointer/J03RemoveDuplicate.java
+++ 14TwoPointer/J03RemoveDuplicate.java
@@ -31,6 +31,21 @@ public class J03RemoveDuplicate {
         return idx;
     }
 
+    // Expected Approach - O(n) Time and O(1) Space
+    public static int usingTwoPointer2(int[] arr){
+        int n = arr.length;
+        if (n <= 1)
+            return n;
+		
+        int idx = 1; 
+      
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[idx++] = arr[i];
+            }
+        }
+        return idx;
+    }
     public static void main(String args[]) {
         int[] arr = { 1, 2, 2, 3, 4, 4, 4, 5, 5 };
         int idx = usingTwoPointer(arr);
@@ -42,7 +57,15 @@ public class J03RemoveDuplicate {
         int[] arr2 = { 1, 2, 2, 3, 4, 4, 4, 5, 5 };
         idx = usingHashing(arr2);
         for(int i = 0;i<idx;i++){
-            System.out.print(arr[i]+"  ");
+            System.out.print(arr2[i]+"  ");
         }
+        System.out.println();
+
+        int[] arr3 = { 1, 2, 2, 3, 4, 4, 4, 5, 5 };
+        idx = usingTwoPointer2(arr3);
+        for(int i = 0;i<idx;i++){
+            System.out.print(arr3[i]+"  ");
+        }
+        System.out.println();
     }
 }
