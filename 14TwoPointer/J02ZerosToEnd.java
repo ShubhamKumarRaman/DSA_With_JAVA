e77c9d1094fe992ce6e6ba6e976c39b52d214817
Shubham Kumar <shubham620409@gmail.com>
2026-05-01 13:24:46 +0530

    Two Pointer approach

--- 14TwoPointer/J02ZerosToEnd.java
+++ 14TwoPointer/J02ZerosToEnd.java
@@ -12,7 +12,7 @@ public class J02ZerosToEnd {
                 j++;
             }
         }
-        while(j<n){
+        while (j < n) {
             temp[j++] = 0;
         }
 
@@ -21,10 +21,30 @@ public class J02ZerosToEnd {
         }
     }
 
+    // [Better Approach] Two Traversals-O(n) Time and O(1) space
+    public static void usingTwoTraversal(int[] arr) {
+        int n = arr.length;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        while (j < n) {
+            arr[j++] = 0;
+        }
+    }
+
     public static void main(String args[]) {
         int[] arr = { 1, 2, 0, 4, 3, 0, 5, 0 };
         usingTempArray(arr);
 
         System.out.println(Arrays.toString(arr));
+
+        int[] arr2 = { 1, 2, 0, 4, 3, 0, 5, 0 };
+        usingTwoTraversal(arr2);
+        System.out.println(Arrays.toString(arr2));
+
     }
 }
