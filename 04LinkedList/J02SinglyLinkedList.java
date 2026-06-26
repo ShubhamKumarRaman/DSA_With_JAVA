624cc9eaf78b1d90b27881922c432d8d4b76d846
Shubham Kumar <shubham620409@gmail.com>
2026-06-26 21:02:20 +0530

    Create J02SinglyLinkedList.java

---
+++ 04LinkedList/J02SinglyLinkedList.java
@@ -0,0 +1,16 @@
+public class J02SinglyLinkedList {
+    public class Node {
+        int data;
+        Node next;
+
+        // Constructor
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
