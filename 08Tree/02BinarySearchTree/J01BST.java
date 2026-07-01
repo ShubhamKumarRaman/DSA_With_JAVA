b7048d6331fcdca2634540bb8d13afa5c9512b7a
Shubham Kumar <shubham620409@gmail.com>
2026-07-01 17:11:32 +0530

    Create J01BST.java

---
+++ 08Tree/02BinarySearchTree/J01BST.java
@@ -0,0 +1,17 @@
+public class J01BST {
+    public static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    public static void main(String args[]) {
+
+    }
+}
