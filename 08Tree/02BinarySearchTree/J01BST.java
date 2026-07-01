067c6e8a953347ff86f5e6563a037b4e94c92932
Shubham Kumar <shubham620409@gmail.com>
2026-07-01 17:16:36 +0530

    Insert new node

--- 08Tree/02BinarySearchTree/J01BST.java
+++ 08Tree/02BinarySearchTree/J01BST.java
@@ -11,6 +11,23 @@ public class J01BST {
 
     Node root;
 
+    // Insert Node
+    public static Node insertNode(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (data < root.data) {
+            root.left = insertNode(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertNode(root.right, data);
+        } else {
+            System.out.println("Duplicate node is not allowed.");
+        }
+
+        return root;
+    }
+
     public static void main(String args[]) {
 
     }
